7b1498a2dfb971c477f4d067efa5e1c8cee1c85b
TheVipek <thevipek0552@gmail.com>
2024-03-25 22:11:37 +0100

    test chart

--- PomoPeak/pomopeakstats.cpp
+++ PomoPeak/pomopeakstats.cpp
@@ -1,5 +1,6 @@
 #include "pomopeakstats.h"
 #include "ui_pomopeakstats.h"
+#include <QToolTip>
 PomopeakStats::PomopeakStats(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::PomopeakStats)
@@ -10,6 +11,8 @@ PomopeakStats::PomopeakStats(QWidget *parent)
     connect(ui->monthlyBtn, &QPushButton::clicked, this, &PomopeakStats::OnViewButtonsClick);
     connect(ui->weeklyBtn, &QPushButton::clicked, this, &PomopeakStats::OnViewButtonsClick);
     SwitchViewToWeekly();
+    InitializeChart();
+
 }
 
 
@@ -49,4 +52,29 @@ void PomopeakStats::SwitchViewToWeekly()
 {
     ui->weeklyBtn->setEnabled(false);
     ui->monthlyBtn->setEnabled(true);
+}
+void PomopeakStats::InitializeChart()
+{
+    chart = new QChart();
+    chart->setTitle("Title");
+    QBarSet* tasksCount = new QBarSet("Task Count");
+    *tasksCount  << 1 << 2 << 3 << 4 << 5;
+
+    QBarSet* timeSpent = new QBarSet("Time Spent");
+    *timeSpent << 1 << 2 << 3 << 4 << 5;
+    QBarSeries* series = new QBarSeries();
+
+    series->append(tasksCount );
+    series->append(timeSpent);
+    QObject::connect(series, &QBarSeries::hovered, [=](bool status, int index, QBarSet *set) {
+        if (status) {
+            // Assuming you want to show the set name and value in the tooltip
+            QToolTip::showText(QCursor::pos(), QString("%1: %2").arg(set->label()).arg(set->at(index)));
+        }
+    });
+    chart->addSeries(series);
+    QChartView* view = new QChartView(chart);
+    ui->chartLayout->addWidget(view);
+
+
 }
--- PomoPeak/pomopeakstats.h
+++ PomoPeak/pomopeakstats.h
@@ -2,7 +2,11 @@
 #define POMOPEAKSTATS_H
 
 #include <QWidget>
-
+#include <QtCharts/QChartView>
+#include <QtCharts/QBarSeries>
+#include <QtCharts/QBarSet>
+#include <QtCharts/QLegend>
+#include <QtCharts/QBarCategoryAxis>
 namespace Ui {
 class PomopeakStats;
 }
@@ -21,9 +25,11 @@ private slots:
     void OnExitClicked();
 private:
     Ui::PomopeakStats *ui;
+    QChart* chart;
     void OnViewButtonsClick();
     void SwitchViewToMonthly();
     void SwitchViewToWeekly();
+    void InitializeChart();
 };
 
 #endif // POMOPEAKSTATS_H
