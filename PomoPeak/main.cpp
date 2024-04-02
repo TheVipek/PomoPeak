d2e69f076257b799282a223eac7c8d1950d4291d
TheVipek <thevipek0552@gmail.com>
2024-04-03 00:18:22 +0200

    set window icon

--- PomoPeak/main.cpp
+++ PomoPeak/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     PomoPeak w;
+    w.setWindowIcon(QIcon(":/icon/images/icon.jpg"));
     w.show();
     QApplication::setQuitOnLastWindowClosed(false);
     return a.exec();
